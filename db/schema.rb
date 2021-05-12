7fc9ea234e47d0b1a93fbb199404b93b9d770925
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-12 06:08:44 +0530

    Add Job model and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_001441) do
+ActiveRecord::Schema.define(version: 2021_05_12_003615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.text "location"
+    t.float "salary"
+    t.string "role"
+    t.text "skills"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
