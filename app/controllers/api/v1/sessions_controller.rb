aed6dc91f5d6ea4e3a10f928dd748fb47b18c60e
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-15 09:41:32 +0530

    Add message response for session create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,7 +7,8 @@ class Api::V1::SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       login!
       render json: { logged_in: true,
-                     user: @user }
+                     user: @user,
+                    message: `User has logged in successfully` }
     else
       render json: { status: 401,
                      errors: ['no such user, please try again'] }
