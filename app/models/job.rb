e4a39a151de619c4556c5de95795e4f4e1cf59f5
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-20 11:26:52 +0530

    Fix issue with App table not updating with new job added

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,8 +3,9 @@ class Job < ApplicationRecord
   has_many :users, through: :apps
 
   def add_recruiter_job_to_apps(user, job)
-    Apps.create!(status: 'recruiter_added',
+    byebug
+    App.create!(status: 'recruiter_added',
                 user_id: user.id,
-                job: job.id)
+                job_id: job.id)
   end
 end
