526cd245121126104c37954ef88b52ba076383a1
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-24 09:44:38 +0530

    Add create method in apps controller

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -3,9 +3,29 @@ class Api::V1::AppsController < ApplicationController
 
   def show; end
 
-  def create; end
+  def create
+    ap = App.new
+    if ap.app_add(app_params)
+      render json: {
+        status: 200,
+        message: 'You have applied for the role'
+      }
+    else
+      render json: {
+        errors: @app.errors.full_messages,
+        status: 500,
+        message: 'You have already applied for the Job'
+      }
+    end
+  end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def app_params
+    params.require(:app).permit(:user_id, :job_id)
+  end
 end
--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,16 @@
 class App < ApplicationRecord
   belongs_to :user
   belongs_to :job
+
+  def app_add(_data)
+    user = user_id
+    job = job_id
+    res = App.where(user_id: user, job_id: job)
+    if res
+      false
+    else
+      App.create(user_id: user, job_id: job)
+      true
+    end
+  end
 end
