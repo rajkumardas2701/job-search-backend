30fa5909a85c2248e63932a49c404e355c305613
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-26 11:25:32 +0530

    Add production domain in cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://job-search-by-raj.netlify.app/'
+    origins 'localhost:3000', 'netlify.app'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: false
   end
 end
-
+# 'https://job-search-by-raj.netlify.app'
 # Rails.application.config.action_controller.forgery_protection_origin_check = false
