b17d0768dc1a43948bfe69046d3d27641577298c
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-26 10:38:46 +0530

    Update CORS of the frontend app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_job-search-backend', domain: 'job-search-backend-json-api'
+  Rails.application.config.session_store :cookie_store, key: '_job-search-backend', domain: 'https://jobhub2701-backend.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_job-search-backend'
 end
