20a29c95d26c0ca03fa0004222aa41cdb4ead65f
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-26 12:06:40 +0530

    add origin of locahost and react frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'netlify.app'
+    origins 'localhost:3000', 'job-search-by-raj.netlify.app'
 
     resource '*',
       headers: :any,
