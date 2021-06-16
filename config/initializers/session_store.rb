6eba7a2dffa047b20e933f697eedba51c8f824ed
Rajkumar Das <rajkumardas2701@gmail.com>
2021-06-16 07:33:00 +0530

    Add double equals for prod

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if Rails.env === 'production' 
+if Rails.env == 'production' 
   Rails.application.config.session_store :cookie_store, key: '_job-search-backend', domain: 'jobhub2701-backend.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_job-search-backend'
