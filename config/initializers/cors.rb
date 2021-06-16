f0ab1fd7402054f302e360e4c713b557c710f575
Rajkumar Das <rajkumardas2701@gmail.com>
2021-06-16 08:51:16 +0530

    Update session store

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,16 +7,15 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://job-search-by-raj.netlify.app'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
-
   allow do
-    origins 'https://job-search-by-raj.netlify.app'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
