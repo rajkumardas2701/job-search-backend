84be9d6cdff374e80ef349496f47075bce1adbfb
Rajkumar Das <rajkumardas2701@gmail.com>
2021-06-18 06:53:29 +0530

    update cookie_store to mention same_site :none and secure: true

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,13 @@
 # if Rails.env == 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_job-search-backend', domain: :all
+  Rails.application.config.session_store :cookie_store, { 
+    :key => '_job-search-backend',
+    :domain => :all,
+    :same_site => :none,
+    :secure => :true,
+    :tld_length => 2
+  }
+  
+  # key: '_job-search-backend', domain: :all
 # else
   # Rails.application.config.session_store :cookie_store, key: '_job-search-backend'
 # end
