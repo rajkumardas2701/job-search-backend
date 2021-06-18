3dfc2b6373e37f6dc3cb37fca5c1bde0f18d0e33
Rajkumar Das <rajkumardas2701@gmail.com>
2021-06-18 06:08:24 +0530

    set domain all in session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # if Rails.env == 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_job-search-backend', domain: 'jobhub2701-backend.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_job-search-backend', domain: :all
 # else
   # Rails.application.config.session_store :cookie_store, key: '_job-search-backend'
 # end
