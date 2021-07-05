5a91f5eabaa015bf78d570f18fd8b073f9aa26cc
Rajkumar Das <rajkumardas2701@gmail.com>
2021-07-05 17:36:03 +0530

    Add fix with authentication

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,25 +1,43 @@
 class Api::V1::SessionsController < ApplicationController
+  # def create
+    # @user = User.find_by(email: session_params[:email])
+    # if @user&.authenticate(session_params[:password])
+    #   login!
+    #   render json: { logged_in: true,
+    #                  user: @user.attributes.except('password_digest'),
+    #                  message: ['User has logged in successfully'] }
+    # else
+    #   render json: { status: 401,
+    #                  errors: ['Invalid Username or Password'] },
+    #          status: 401
+    # end
+  # end
+
   def create
+    auth_object = Authentication.new(session_params)
     @user = User.find_by(email: session_params[:email])
-    if @user&.authenticate(session_params[:password])
-      login!
-      render json: { logged_in: true,
-                     user: @user.attributes.except('password_digest'),
-                     message: ['User has logged in successfully'] }
+    if auth_object.authenticate
+      render json: {
+        logged_in: true,
+        user: @user.attributes.except('password_digest'),
+        message: ['User has logged in successfully'],
+        token: auth_object.generate_token
+      }
     else
-      render json: { status: 401,
-                     errors: ['Invalid Username or Password'] },
-             status: 401
+      render json: {
+        message: 'Incorrect username/password combination'
+      },
+             status: :unauthorized
     end
   end
 
-  def destroy
-    logout!
-    render json: {
-      status: 200,
-      logged_out: true
-    }
-  end
+  # def destroy
+  #   logout!
+  #   render json: {
+  #     status: 200,
+  #     logged_out: true
+  #   }
+  # end
 
   private
 
